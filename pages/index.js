af78625854a9694e887917e9d72cad6e6ebe6750
“Raqib <raqib850@gmail.com>
2023-10-13 12:27:47 +0530

    homepage started

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,37 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+import Navbar from "../components/ui-elements/nav-bar/navbar";
 
 
-
-function Home() {
+const Home = () => {
   const router = useRouter();
   const { t } = useTranslation("common");
 
   return (
     <div>
-      Hello world!
+      <Navbar />
+
+      <div className="hero_wrapper">
+
+        <img src="/images/generic/hero-img.jpeg" alt="" />
+
+        <Overlay />
+
+      </div>
     </div>
   );
+}
+
+
+const Overlay = () => {
+  return (
+    <div className="overlay_wrapper">
+      <div className="text white_text bold_text font20 screen_margin">lorem ipsum dolor sit amet</div>
+      <div className="text white_text bold_text font20 screen_margin">lorem ipsum dolor sit amet</div>
+      <div className="text white_text bold_text font20 screen_margin">lorem ipsum dolor sit amet</div>
+    </div>
+  )
 }
 
 
