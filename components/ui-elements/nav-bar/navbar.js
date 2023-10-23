2455669ebfca91d17e3b59ef8742b6b7d07a2280
sameerhamid <sameerhamid213.com>
2023-10-23 11:15:39 +0530

    added onclick on signin

--- components/ui-elements/nav-bar/navbar.js
+++ components/ui-elements/nav-bar/navbar.js
@@ -1,6 +1,8 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Navbar = () => {
+    const router = useRouter()
     return (
         <div className="navbar_wrapper">
             <div className="logo_wrapper">
@@ -10,6 +12,7 @@ const Navbar = () => {
                 <div className='link'>Home</div>
                 <div className='link'>About us</div>
                 <div className='link'>Contact us</div>
+                <div className='link' onClick={() => router.push("signin")}>Sign in</div>
             </div>
         </div>
     )
